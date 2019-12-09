f5885ca91081759a8b517352f8a820c6d72ccdb2
joelpdurham <joeldur@gmail.com>
2019-12-09 14:08:20 -0800

    passed first test for parseRequest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,8 @@
-const request = require('supertest');
-const app = require('../lib/app');
+// const request = require('supertest');
+// const app = require('../lib/app');
 
 describe('createResponse', () => {
-
+    it('will run a test when Im ready to test', () => {
+        expect(1).toEqual(1);
+    });
 });
