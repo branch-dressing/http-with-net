136f62f504cfafe99278e233c0b96d586cf22fc4
joelpdurham <joeldur@gmail.com>
2019-12-09 15:29:11 -0800

    post route works

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,6 +7,13 @@ describe('createResponse', () => {
             .get('/')
             .then(res => {
                 expect(res.text).toEqual('hi');
-            })
+            });
+    });
+    it('returns status code 200 and plain text with req body', () => {
+        return request(app)
+            .post('/echo')
+            .then(res => {
+                expect(res.text).toEqual('post');
+            });
     });
 });
